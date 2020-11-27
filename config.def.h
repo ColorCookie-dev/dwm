b44f0d93773c1fa3e32cc82b67180808b0ea0e81
Color Cookie <colorcookie@protonmail.com>
2020-11-27 11:06:05 +0530

    Terminal -> uxterm, st crashes when viewing emojis

--- config.def.h
+++ config.def.h
@@ -67,7 +67,7 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-#define TERMINAL "st"
+#define TERMINAL "uxterm"
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_crimson, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { TERMINAL, NULL };
