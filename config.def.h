e59f89332b43c23448bfacaf00d37eb3ccd90577
Color Cookie <colorcookie@protonmail.com>
2020-11-27 06:04:52 +0530

    Color Change to dmenu_run

--- config.def.h
+++ config.def.h
@@ -69,7 +69,7 @@ static const Layout layouts[] = {
 /* commands */
 #define TERMINAL "uxterm"
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_orange, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_crimson, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { TERMINAL, NULL };
 
 static Key keys[] = {
